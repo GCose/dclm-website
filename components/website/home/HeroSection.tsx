6b2890aa8cbaf4c6934964155c43fe78a123b31c
Goodness Adewuyi <goodnessadewuyi8@gmail.com>
2025-10-24 14:21:13 +0000

    Made changes

--- components/website/home/HeroSection.tsx
+++ components/website/home/HeroSection.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
-export default function HeroSection() {
+const HeroSection = () => {
   return (
     <section className="min-h-screen pt-25 pb-20 px-6 md:px-8 bg-cream">
       <div className="mx-auto">
-        <h1 className="text-[clamp(3rem,12vw,8rem)] font-semibold leading-[1.2] tracking-tight mb-10">
+        <h1 className="text-[clamp(3rem,12vw,9rem)] font-semibold leading-[1.2] tracking-tight mb-10">
           WELCOME TO DCLM
           <br />
           BRIKAMA REGION
@@ -21,13 +21,20 @@ export default function HeroSection() {
         </div>
 
         <div className="max-w-4xl ml-auto">
-          <p className="text-[clamp(1.2rem,2vw,1.9rem)] leading-[1.7] tracking-wider text-black">
-            A vibrant community of believers in Brikama, The Gambia, united in
-            faith and fellowship. With a commitment to holiness and spiritual
-            growth, we create a space where faith meets life.
+          <p className="text-[clamp(1.5rem,4vw,2.5rem)] leading-[1.1] text-black">
+            <span className="pl-30">
+              A vibrant community of believers in Brikama
+            </span>{" "}
+            <span>
+              united in faith and fellowship. With a commitment to holiness and
+              spiritual growth. We prioritize righteous living amongst ourselves
+              and ensure total commitment and concescration to GOD.
+            </span>
           </p>
         </div>
       </div>
     </section>
   );
-}
+};
+
+export default HeroSection;
