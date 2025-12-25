3227f79d8c790ff2e63686b4fdcc01b474364998
GCose <goodnessadewuyi8@gmail.com>
2025-12-25 13:39:14 +0000

    Made changes

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
+import Footer from "@/components/website/layout/Footer";
+import Layout from "@/components/website/layout/Layout";
 import CTASection from "@/components/website/CTASection";
 import LoadingScreen from "@/components/website/LoadingScreen";
 import HeroSection from "@/components/website/events/HeroSection";
-import Footer from "@/components/website/layout/Footer";
 
 export default function Events() {
   const [loading, setLoading] = useState(true);
 
   return (
-    <Layout title="Events | View All Events" description="Surf through all services organkzed by the church.">
+    <Layout
+      title="Events | View All Events"
+      description="Surf through all services organkzed by the church."
+    >
       {loading && (
         <LoadingScreen
           title="Explore all events we've had"
